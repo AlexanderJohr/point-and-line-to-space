2bc17ca6c07a51ad0d3dd08edd2a568ec7f36083
maphy <syoyosoccer10@gmail.com>
2019-02-10 21:38:16 +0900

    I made UI support multiple screens.

--- Point and Line to Space/Assets/Scripts/Button.cs
+++ Point and Line to Space/Assets/Scripts/Button.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Button : MonoBehaviour {
-    public  string MenuSelect;
-    public void MenuDisplay(){
-        GameObject.Find("Menu").transform.Find(MenuSelect).gameObject.SetActive(true);
-        GameObject.Find("Menu").transform.Find("Buttons").gameObject.SetActive(false);
+
+    //bool isMenuDisplay =true;
+    public  string SelectMenuName;
+
+    public void BackTitle(){
+        GameObject.Find("Canvas").transform.Find("Menu").gameObject.SetActive(false);
+        GameObject.Find("Canvas").transform.Find("Title").gameObject.SetActive(true);
+        GameObject.Find("Canvas").transform.Find("TapScreenToStart").gameObject.SetActive(true);
     }
-    public void MenuDelete(){
+
+    public void MenuDisplay(){
+
+        GameObject.Find("MenuPanel").transform.Find(SelectMenuName).gameObject.SetActive(true);
+        /*if(isMenuDisplay){
+            print("Success");
+            isMenuDisplay = false;
+            GameObject.Find("MenuPanel").transform.Find(SelectMenuName).gameObject.SetActive(true);
+        }
+        else{
+            print("Error");
+        }*/
+
+    }
+
+
+    public void Return(){
+        //isMenuDisplay = true;
         this.gameObject.SetActive(false);
-        GameObject.Find("Menu").transform.Find("Buttons").gameObject.SetActive(true);
+        /*if(isMenuDisplay){
+            print("isMenueDispalay is true");
+        }
+        else{
+            print("isMenueDispalay is false");
+        }*/
     }
 
 }
--- Point and Line to Space/Assets/Scripts/MenuTriger.cs
+++ Point and Line to Space/Assets/Scripts/MenuTriger.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class MenuTriger: MonoBehaviour {
     // Update is called once per frame
     void Update () {
-        if (Input.touchCount > 0)
+        
+        if ((Input.touchCount > 0))
         {
             Touch touch = Input.GetTouch(0);
 
             if (touch.phase == TouchPhase.Began)
             {
-                GameObject.Find("GameObject").transform.Find("Menu").gameObject.SetActive(true);
+                print("Do");
+                GameObject.Find("Canvas").transform.Find("Menu").gameObject.SetActive(true);
+                GameObject.Find("Canvas").transform.Find("Title").gameObject.SetActive(false);
+                GameObject.Find("Canvas").transform.Find("TapScreenToStart").gameObject.SetActive(false);
             }
         }
     }
