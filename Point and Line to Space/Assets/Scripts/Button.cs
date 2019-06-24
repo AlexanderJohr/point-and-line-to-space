7528635945b4ca37ecaa5ecee23eaa98c8e7e641
AlexanderJohr <a.johr@lizardis.de>
2019-06-24 23:34:29 +0200

    Revert "Merge branch 'master' of https://github.com/AlexanderJohr/point-and-line-to-space"
    This reverts commit b1b359266794f860c8d2868b7fd5ba574a87b38b, reversing
    changes made to 7d6b3745464c768a275cbe31e988d26807376483.

---
+++ Point and Line to Space/Assets/Scripts/Button.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    //bool isMenuDisplay =true;
+    public  string SelectMenuName;
+
+    public void BackTitle(){
+        GameObject.Find("Canvas").transform.Find("Menu").gameObject.SetActive(false);
+        GameObject.Find("Canvas").transform.Find("Title").gameObject.SetActive(true);
+        GameObject.Find("Canvas").transform.Find("TapScreenToStart").gameObject.SetActive(true);
+    }
+
+    public void MenuDisplay(){
+
+        GameObject.Find("MenuPanel").transform.Find(SelectMenuName).gameObject.SetActive(true);
+        /*if(isMenuDisplay){
+            print("Success");
+            isMenuDisplay = false;
+            GameObject.Find("MenuPanel").transform.Find(SelectMenuName).gameObject.SetActive(true);
+        }
+        else{
+            print("Error");
+        }*/
+
+    }
+
+
+    public void Return(){
+        //isMenuDisplay = true;
+        this.gameObject.SetActive(false);
+        /*if(isMenuDisplay){
+            print("isMenueDispalay is true");
+        }
+        else{
+            print("isMenueDispalay is false");
+        }*/
+    }
+
+}
